26794d7cf0cb631c39932fc6775ab1fd6613c17d
katie stout <katiestout@katies-MBP.home>
2018-07-22 16:06:15 -0400

    got onclick to work

--- theclickygame/src/App.js
+++ theclickygame/src/App.js
@@ -1,31 +1,56 @@
-import React, {Component} from "react";
+import React from "react";
 //import { BrowserRouter as Router, Route } from "react-router-dom";
 //import Home from "./components/pages/Home";
 import PusheenList from "./PusheenList.json";
 import PusheenCard from "./components/PusheenCard/PusheenCard";
 import Wrapper from "./components/Wrapper/Wrapper";
+import NavBar from "./components/Navbar/Navbar"
 //import PusheenImages from "../public/PusheenImages"; 
 
 
-class App extends Component{
+class App extends React.Component{
   //sets this.state.pusheenlist to the pusheenlist json array 
+  constructor(props){
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick(){
+    
+    console.log("yahoo");
+  };
+
   state = {
     PusheenList,
+   // score: 0,
   };
+
+
+
+//this.onClick = this.handleClick.bind(this);
+
+//gave the name a key, must not be identical when mapping over a list
 
 //get all of the pusheens using map
 render(){
   return(
     <Wrapper>
+
+      <NavBar/>
+  
+
     {this.state.PusheenList.map(PusheenList => (
-        
-        <PusheenCard
+
+      <div onClick = {this.handleClick}>
+        <PusheenCard 
         name key= {PusheenList.name}
-        image = {PusheenList.image}
+        image = {PusheenList.image} 
+        //onClick= {this.handleClick}
         />
-
-
+        </div>
     ))}
+
+    
+    
     </Wrapper>
   );
 };
--- theclickygame/src/components/PusheenCard/PusheenCard.js
+++ theclickygame/src/components/PusheenCard/PusheenCard.js
@@ -1,25 +1,28 @@
 import React from "react";
 import "./PusheenCss.css";
 
-//import PusheenList from "../../../../theclickygame/src/PusheenList.json";
 
+
+//import PusheenList from "../../../../theclickygame/src/PusheenList.json";
+//changed alt to true!
 const PusheenCard = props =>(
+
   <div className ="card">
   <div className = "img-container">
-
-  <img alt="true" src ={props.image} />
+  <img alt="true" src ={props.image}/>
 
   
   </div>
   <div className="content">
    
     <p>
-   
+   each
     </p> 
 
     
     </div>
   </div>
 );
+
 
 export default PusheenCard;
